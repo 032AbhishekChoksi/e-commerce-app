b6b967362f42c60795589a9fb2e6f5f4046e1a92
032AbhishekChoksi <19bmiit032@gmail.com>
2025-02-07 22:39:55 +0530

    implement eureka server and config server

---
+++ services/config-server/src/test/java/com/abhishekchoksi/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishekchoksi.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/discovery/src/test/java/com/abhishekchoksi/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishekchoksi.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
